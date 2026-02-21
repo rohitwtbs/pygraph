2981071aa927d9390fe02e996bb2ba3cf43b0436
rohit kumar <12rohit4@gmail.com>
2026-02-21 19:48:44 +0000

    "input correction"

--- input.py
+++ input.py
@@ -1,25 +1,22 @@
 import streamlit as st
-import numpy as np
-import pandas as pd
 import sympy as sp
 
 
-class InputBox():
-    def __init__(self, default="sinx(x)"):
-        self.expr_text = st.text_input("Enter f(x)", value=default)
-        self.allowed = {
-            "x": sp.symbols("x)"),
+class InputBox:
+    def __init__(self, default="sin(x)"):
+        self._allowed = {
+            "x": sp.symbols("x"),
             "sin": sp.sin,
             "cos": sp.cos,
             "tan": sp.tan,
             "exp": sp.exp,
+            "log": sp.log,
+            "sqrt": sp.sqrt,
+            "pi": sp.pi,
+            "abs": sp.Abs,
         }
-        pass
+        self.expr_text = st.text_input("Enter f(x)", value=default)
 
     def parse(self):
         expr = sp.sympify(self.expr_text, locals=self._allowed)
         return sp.lambdify(sp.symbols("x"), expr, modules=["numpy"])
-
-    def __eq__(self, other):
-        return self.expr_text == other
-    
