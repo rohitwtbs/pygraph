1d8dcd7d489399f56cc56fc98d3213a778010862
rohit kumar <12rohit4@gmail.com>
2026-02-21 20:04:35 +0000

    "adding plotly for smoother graph zoom in and zoom out"

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
+import plotly.graph_objects as go
 
-st.set_page_config(page_title="Simple Calculator")
+st.set_page_config(page_title="PyGraph", layout="wide")
 st.title("📈 PyGraph")
-
 
 equation = st.selectbox("Select Function", ["sin", "cos", "tan"])
 freq = st.slider("Frequency", 1, 10, 1)
 
 
-x = np.linspace(0, 2 * np.pi, 500)
+if "x_range" not in st.session_state:
+    st.session_state.x_range = [-10, 10]
+if "y_range" not in st.session_state:
+    st.session_state.y_range = [-5, 5]
+
+x_min, x_max = st.session_state.x_range
+x = np.linspace(x_min, x_max, 3000)
+
 if equation == "sin":
     y = np.sin(freq * x)
 elif equation == "cos":
@@ -18,11 +24,57 @@ elif equation == "cos":
 else:
     y = np.tan(freq * x)
 
+y = np.array(y, dtype=float)
+y[~np.isfinite(y)] = np.nan
 
-chart_data = pd.DataFrame({
-    'x': x,
-    'y': y
-}).set_index('x')
+fig = go.Figure()
+fig.add_trace(go.Scatter(
+    x=x, y=y,
+    mode="lines",
+    line=dict(color="#1f77b4", width=2),
+    name=equation
+))
+
+fig.update_layout(
+    dragmode="pan",
+    xaxis=dict(
+        range=st.session_state.x_range,
+        showgrid=True,
+        zeroline=True,
+        zerolinecolor="black",
+        zerolinewidth=2,
+        gridcolor="lightgrey",
+    ),
+    yaxis=dict(
+        range=st.session_state.y_range,
+        showgrid=True,
+        zeroline=True,
+        zerolinecolor="black",
+        zerolinewidth=2,
+        gridcolor="lightgrey",
+    ),
+    plot_bgcolor="white",
+    margin=dict(l=0, r=0, t=30, b=0),
+    height=600,
+)
+
+event = st.plotly_chart(
+    fig,
+    use_container_width=True,
+    config={
+        "scrollZoom": True,
+        "displayModeBar": True,
+        "modeBarButtonsToRemove": ["select2d", "lasso2d"],
+    },
+    on_select="rerun",
+    key="graph"
+)
 
 
-st.line_chart(chart_data)
+if event and "layout" in event:
+    layout = event["layout"]
+    if "xaxis.range[0]" in layout:
+        st.session_state.x_range = [layout["xaxis.range[0]"], layout["xaxis.range[1]"]]
+    if "yaxis.range[0]" in layout:
+        st.session_state.y_range = [layout["yaxis.range[0]"], layout["yaxis.range[1]"]]
+    st.rerun()
