350c4d9368c270995b4ac1f9f54f000a8c8a8758
rohit kumar <12rohit4@gmail.com>
2026-02-21 12:07:26 +0000

    "adding requirements file"

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import pandas as pd
 st.set_page_config(page_title="Simple Calculator")
 st.title("📈 PyGraph")
 
-# 1. Inputs using Streamlit widgets
+
 equation = st.selectbox("Select Function", ["sin", "cos", "tan"])
 freq = st.slider("Frequency", 1, 10, 1)
 
-# 2. Generate Data
+
 x = np.linspace(0, 2 * np.pi, 500)
 if equation == "sin":
     y = np.sin(freq * x)
@@ -18,12 +18,11 @@ elif equation == "cos":
 else:
     y = np.tan(freq * x)
 
-# 3. Streamlit charts need data in a DataFrame
+
 chart_data = pd.DataFrame({
     'x': x,
     'y': y
 }).set_index('x')
 
-# 4. Plot using the native line chart
-# This is automatically responsive to screen size
+
 st.line_chart(chart_data)
